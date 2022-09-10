c4f4b2b5e1f0f7d8f9259baa93cbd44bea0d37dc
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-09-10 19:57:34 +0700

    add clases for identify this user is admin or not

--- src/Commands/Group/Executor/Pin.cs
+++ src/Commands/Group/Executor/Pin.cs
@@ -1,6 +1,7 @@
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot;
+using Telegram.Bot.Exceptions;
 using Prtscbot.Utils;
 
 namespace Prtscbot.Commands.Group.Executor
@@ -18,9 +19,12 @@ namespace Prtscbot.Commands.Group.Executor
                 }
                 public async Task Execute()
                 {
+                        AdminCheck admincheck = new AdminCheck(message.From.Id);
+                        
                         if (!this.checkIsReply(message))
                         {
 
+                                
                                 string text = TranslateLocale.exec(
                                         message, "command.Group.Pin.NeedReply"
                                 );
@@ -32,12 +36,37 @@ namespace Prtscbot.Commands.Group.Executor
                         }
                         else
                         {
-                                string text = "pin ready";
-                                await botClient.SendTextMessageAsync(
-                                        chatId: message.Chat.Id,
-                                        text: text,
-                                        parseMode: ParseMode.Html
-                                );
+                                try {
+                                        await botClient.PinChatMessageAsync(
+                                                chatId: message.Chat.Id,
+                                                messageId: message.ReplyToMessage.MessageId
+                                        );
+                                        string text = TranslateLocale.exec(
+                                                message, "command.Group.Pin.Success"
+                                        );
+                                        await botClient.SendTextMessageAsync(
+                                                chatId: message.Chat.Id,
+                                                text: text,
+                                                parseMode: ParseMode.Html
+                                        );
+                                } catch (ApiRequestException) {
+                                        string text = TranslateLocale.exec(
+                                                message, "command.Group.Pin.NotEnoughPermission"
+                                        );
+                                        await botClient.SendTextMessageAsync(
+                                                chatId: message.Chat.Id,
+                                                text: text,
+                                                parseMode: ParseMode.Html
+                                        );
+                                }
+                                // string text = TranslateLocale.exec(
+                                //         message, "command.Group.Pin.Success"
+                                // );
+                                // await botClient.SendTextMessageAsync(
+                                //         chatId: message.Chat.Id,
+                                //         text: text,
+                                //         parseMode: ParseMode.Html
+                                // );
                         }
 
                 }
---
+++ src/Utils/AdminCheck.cs
@@ -0,0 +1,16 @@
+namespace Prtscbot.Utils {
+        class AdminCheck
+        {
+                private long user_Id;
+                public AdminCheck(long user_Id)
+                {
+                        this.user_Id = user_Id;
+                }
+
+                // public bool isAdmin()
+                // {
+                        
+                // }
+
+        }
+}
