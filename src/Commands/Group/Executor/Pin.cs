b1930bca6954beaa482678a86c1e4f0817b3a462
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-09-10 10:48:47 +0700

    add pin

--- src/Commands/Group/Executor/Pin.cs
+++ src/Commands/Group/Executor/Pin.cs
@@ -1,16 +1,44 @@
-// using Telegram.Bot.Types;
-// using Telegram.Bot.Types.Enums;
-// using Telegram.Bot;
-// using Prtscbot.Utils;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot;
+using Prtscbot.Utils;
 
+namespace Prtscbot.Commands.Group.Executor {
+        class Pin
+        {
+                private InputTelegram inputTelegram;
+                private ITelegramBotClient botClient;
+                private Message message;
+                public Pin(InputTelegram inputTelegram, ITelegramBotClient botClient, Message message)
+                {
+                        this.inputTelegram = inputTelegram;
+                        this.botClient = botClient;
+                        this.message = message;
+                }
+                public async Task Execute()
+                {
+                        if (!this.checkIsReply(message))
+                        {
 
-// namespace Prtscbot.Commands.Group.Executor
-// {
-//         class Pin
-//         {
-//                 public static async Task Execute(InputTelegram inputTelegram, ITelegramBotClient botClient, Message message)
-//                 { 
-//                         await botClient.PinChatMessageAsync()
-//                 }
-//         }
-// }
+                                string text = TranslateLocale.exec(
+                                        message, "command.Group.Pin.NeedReply"
+                                );
+                                await botClient.SendTextMessageAsync(
+                                        chatId: message.Chat.Id,
+                                        text: text,
+                                        parseMode: ParseMode.Html
+                                );
+                        }
+                        
+                }
+                protected bool checkIsReply(Message message)
+                {
+                        if (message.ReplyToMessage == null)
+                        {
+                                return false;
+                        } else {
+                                return true;
+                        }
+                }
+        }
+}
