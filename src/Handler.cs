ed129410eb5cc6668b77b0aecd1e6efdd6f4923d
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-09-14 20:57:07 +0700

    add feature for restrict bot added by other people

--- src/Handler.cs
+++ src/Handler.cs
@@ -9,29 +9,41 @@ namespace fadhil_robot.Program
         {
                 public async Task HandleMessange(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
                 {
-                        await botClient.LeaveChatAsync(message.Chat.Id);
-                        try
+                        bool granted = false;
+                        foreach(makeNegative listall in Config.getWhiteList())
                         {
-                                // avoid messange non text use parsers and caused void
-                                if (message.Text != null)
+                                if (message.Chat.Id == listall.get())
                                 {
-                                        Parse parser = new Parse(message.Text);
-                                        InputTelegram inp = new InputTelegram();
-
-                                        inp.command = parser.getResult()["command"];
-                                        inp.value = parser.getResult()["value"];
-                                        inp.cancellationToken = cancellationToken;
-
-                                        await this.executor(inp, botClient, message);
+                                        granted = true;
                                 }
-
-
-
                         }
-                        catch (Exception e)
+                        if (granted)
                         {
-                                string messange = "exception name: <b>" + e.GetType().Name + "</b>\nmessange: " + e.Message + "\n\ntrace: \n" + e.StackTrace;
-                                await botClient.SendTextMessageAsync(chatId: message.Chat.Id, text: messange, replyToMessageId: message.MessageId, parseMode: ParseMode.Html);
+                                try
+                                {
+                                        // avoid messange non text use parsers and caused void
+                                        if (message.Text != null)
+                                        {
+                                                Parse parser = new Parse(message.Text);
+                                                InputTelegram inp = new InputTelegram();
+
+                                                inp.command = parser.getResult()["command"];
+                                                inp.value = parser.getResult()["value"];
+                                                inp.cancellationToken = cancellationToken;
+
+                                                await this.executor(inp, botClient, message);
+                                        }
+
+
+
+                                }
+                                catch (Exception e)
+                                {
+                                        string messange = "exception name: <b>" + e.GetType().Name + "</b>\nmessange: " + e.Message + "\n\ntrace: \n" + e.StackTrace;
+                                        await botClient.SendTextMessageAsync(chatId: message.Chat.Id, text: messange, replyToMessageId: message.MessageId, parseMode: ParseMode.Html);
+                                }
+                        } else {
+                                await botClient.LeaveChatAsync(message.Chat.Id);
                         }
                 }
 
---
+++ test/negative/Program.cs
@@ -0,0 +1,36 @@
+﻿class makeNegative{
+        private long data;
+        public makeNegative(long data)
+        {
+                this.data = data;
+        }
+
+        public long get()
+        {
+                return this.data;
+        }
+}
+
+
+class main
+{
+        public static void Main()
+        {
+                // makeNegative[] list = new makeNegative[];
+                // list[0] = new makeNegative(-1001410961692);
+
+                // foreach(makeNegative lists in list)
+                // {
+                //         Console.WriteLine(lists.get());
+                // }
+
+                List<makeNegative> lists = new List<makeNegative>();
+                lists.Add(new makeNegative(-1001410961692));
+
+                foreach(makeNegative listall in lists)
+                {
+                        Console.WriteLine(listall.get());
+                }
+
+        }
+}
