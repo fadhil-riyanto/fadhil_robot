fb859f4e33a0d9bf474ccf06db1c7699590c4b24
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-09-15 20:14:16 +0700

    add help command (initial)

---
+++ src/Commands/Private/Executor/Help.cs
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: GPL-2.0
+
+/*
+ *  main.c
+ *  Copyright (C) Fadhil Riyanto
+ *
+ *  https://github.com/fadhil-riyanto/ctg.git
+ */
+
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot;
+using fadhil_robot.Utils;
+
+namespace fadhil_robot.Commands.Private.Executor
+{
+        class Help
+        {
+                private InputTelegram inputTelegram;
+                private ITelegramBotClient botClient;
+                private Message message;
+                public Help(InputTelegram inputTelegram, ITelegramBotClient botClient, Message message)
+                {
+                        this.inputTelegram = inputTelegram;
+                        this.botClient = botClient;
+                        this.message = message;
+                }
+
+                public async Task Execute()
+                {
+                        InlineKeyboardMarkup inlineKeyboard = new(new[]
+                                {
+                                        new []
+                                        {
+                                                InlineKeyboardButton.WithCallbackData(text: "Admins", callbackData: "11"),
+                                                InlineKeyboardButton.WithCallbackData(text: "tes1", callbackData: "2332"),
+                                        },
+                                        new []
+                                        {
+                                                InlineKeyboardButton.WithCallbackData(text: "test2", callbackData: "1"),
+                                                InlineKeyboardButton.WithCallbackData(text: "test3", callbackData: "22"),
+                                        },
+                                }
+                        );
+                        await this.botClient.SendTextMessageAsync(
+                                chatId: this.message.Chat.Id, 
+                                text: "help menu", 
+                                replyMarkup: inlineKeyboard,
+                                replyToMessageId: this.message.MessageId, 
+                                parseMode: ParseMode.Html
+                        );
+
+                }
+        }
+}
