02db13276f2406bc7c500c6437bc8d7a0c74fcf0
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-09-20 20:22:47 +0700

    change wrong Translation

--- src/Utils/Translation.cs
+++ src/Utils/Translation.cs
@@ -32,7 +32,7 @@ namespace fadhil_robot.Utils
                                 "command.Private.Start"  => "Hi, I'm fadhil robot, nice to meet you. I can manage your telegram group with many useful features!",
                                 "command.Private.Start.OwnerTextKeyboard"  => "My Programmer",
                                 "command.Private.Ping"  => "Ping in private chat ok!",
-                                "command.Private.Help"  => "Hai {0}, aku Fadhil Robot.. ini adalah menu pertolongan jika anda tidak paham kegunaanku, semua command bekerja dengan / dan !",
+                                "command.Private.Help"  => "Hi {0}, I'm Fadhil Robot, created by @fadhil_riyanto. this is a help menu if you don't know what I'm using, all commands work with / and !",
 
                                 // group
                                 "command.Group.Ping"  => "Ping in group ok!",
@@ -58,7 +58,7 @@ namespace fadhil_robot.Utils
                                 "command.Private.Start"  => "hai, saya fadhil_robot, senang bertemu denganmu. saya bisa mengatur grup telegram kamu dengan banyak fitur yang berguna!",
                                 "command.Private.Start.OwnerTextKeyboard"  => "Programmer saya",
                                 "command.Private.Ping"  => "Ping di chat pribadi oke!",
-                                "command.Private.Help"  => "Hi {0}, I'm Fadhil Robot, created by @fadhil_riyanto. this is a help menu if you don't know what I'm using, all commands work with / and !",
+                                "command.Private.Help"  => "Hai {0}, aku Fadhil Robot.. ini adalah menu pertolongan jika anda tidak paham kegunaanku, semua command bekerja dengan / dan !",
 
                                 // group
                                 "command.Group.Ping"  => "Ping di grup oke!",
