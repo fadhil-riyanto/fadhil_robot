3e962d278bfc7cf5aefcffd98f0d8fbdf97b4c8a
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-09-20 20:22:38 +0700

    minimize the cb data

--- src/Utils/CallbackHelper.cs
+++ src/Utils/CallbackHelper.cs
@@ -1,31 +1,35 @@
 using Telegram.Bot.Types;
 using Newtonsoft.Json;
+using System.IO.Compression;
+using System;
 
 namespace fadhil_robot.Utils
 {
-        class packtype {
-                public int messange_id {get; set;}
-                public long chat_id {get; set;}
-                public Dictionary<string, object> data {get; set;}
+        class packtype
+        {
+                public int m { get; set; }
+                public long c { get; set; }
+                public Dictionary<string, string> d { get; set; }
 
         }
 
-        class unpacktype {
-                public int messange_id {get; set;}
-                public long chat_id {get; set;}
-                public Dictionary<string, object> data {get; set;}
+        class unpacktype
+        {
+                public int m { get; set; }
+                public long c { get; set; }
+                public Dictionary<string, string> d { get; set; }
 
         }
         class CallbackHelper
         {
-
-                public static string pack(Message message, Dictionary<string, object> val)
+                
+                public static string pack(Message message, Dictionary<string, string> val)
                 {
                         var rareclass = new packtype
                         {
-                                messange_id = message.MessageId,
-                                chat_id = message.Chat.Id,
-                                data = val
+                                m = message.MessageId,
+                                c = message.Chat.Id,
+                                d = val
                         };
 
                         string cokkedstring = System.Text.Json.JsonSerializer.Serialize(rareclass);
