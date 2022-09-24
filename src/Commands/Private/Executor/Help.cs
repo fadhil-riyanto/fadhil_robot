9bd6f85ce97c1788d9884d673edc98b2272eddc8
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-09-24 10:15:37 +0700

    change pack location

--- src/Commands/Private/Executor/Help.cs
+++ src/Commands/Private/Executor/Help.cs
@@ -29,19 +29,15 @@ namespace fadhil_robot.Commands.Private.Executor
 
                 public async Task Execute()
                 {       
-                        string a = CallbackHelper.pack(message, new Dictionary<string, string> {
-                                { "type", "admins"}
-                        });
-
-                        Console.WriteLine(a);
                         InlineKeyboardMarkup inlineKeyboard = new(new[]
                                 {
                                         new []
                                         {
-                                                
                                                 InlineKeyboardButton.WithCallbackData(
                                                         
-                                                        text: "Admins", callbackData: a
+                                                        text: "Admins", callbackData: CallbackHelper.pack(message, inputTelegram,  new Dictionary<string, string> {
+                                                                { "type", "admins"}
+                                                        })
                                                 ),
                                         }
                                 }
