534cb6773294237085ea99843e83eb66513f7d95
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-09-24 08:39:32 +0700

    add type for cb query

--- src/Utils/Type.cs
+++ src/Utils/Type.cs
@@ -9,6 +9,8 @@
 
 using MongoDB.Driver;
 using Telegram.Bot;
+using Telegram.Bot.Types;
+using StackExchange.Redis;
 
 namespace fadhil_robot.Utils {
         class InputTelegram
@@ -17,5 +19,19 @@ namespace fadhil_robot.Utils {
                 public string value;
                 public CancellationToken cancellationToken;
 
+        }
+
+        class InputTelegramCb
+        {
+                public int messange_id { get; set; }
+                public long chat_id { get; set; }
+                public Dictionary<string, string> data { get; set; }
+                public CallbackQuery callback { get; set; }
+
+        }
+
+        class main_thread_ctx
+        {
+                public ConnectionMultiplexer redis { get; set; }
         }
 }
