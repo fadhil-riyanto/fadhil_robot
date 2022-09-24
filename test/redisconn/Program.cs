e75a2415f56f153f991adf82d47695211110167d
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-09-24 10:14:47 +0700

    tests

---
+++ test/redisconn/Program.cs
@@ -0,0 +1,20 @@
+﻿using StackExchange.Redis;
+class ngetes {
+        public ConnectionMultiplexer redisctx;
+        public void makeconn() {
+                this.redisctx = ConnectionMultiplexer.Connect("localhost");
+        }
+        public void set(string key, string value) {
+                IDatabase db = this.redisctx.GetDatabase();
+                db.StringSet(key, value);
+        }
+}
+class redisconn {
+        public static void Main() {
+                ngetes ng = new ngetes();
+                ng.makeconn();
+                for(int a = 0; a < 100000; a++) {
+                        ng.set("test" + a, "banh");
+                }
+        }
+}
---
+++ test/test_csprng/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Security.Cryptography;
+using System;
+using System.Text;
+class generate_rand_str
+{
+        public string csprng(int bytes)
+        {
+                using (RandomNumberGenerator rng = new RNGCryptoServiceProvider())
+                {
+                        byte[] tokenData = new byte[bytes];
+                        rng.GetBytes(tokenData);
+
+                        string token = Convert.ToBase64String(tokenData);
+                        return token;
+                }
+        }
+        public string csprng2(int bytes)
+        {
+                using (var generator = RandomNumberGenerator.Create())
+                {
+                        var salt = new byte[bytes];
+                        generator.GetBytes(salt);
+                        return Convert.ToBase64String(salt);
+                }
+        }
+
+        public string unix()
+        {
+                var now = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0);
+                return now.TotalSeconds.ToString();
+        }
+
+        public string rand_int()
+        {
+                Random rn = new Random();
+                return rn.NextInt64(999999999999999999).ToString();
+        }
+        public string rand_int_range(int range)
+        {
+                Random rn = new Random();
+                return rn.NextInt64(range).ToString();
+        }
+
+        public string sha256(string randomString)
+        {
+                var crypt = new SHA256Managed();
+                string hash = String.Empty;
+                byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(randomString));
+                foreach (byte theByte in crypto)
+                {
+                        hash += theByte.ToString("x2");
+                }
+                return hash;
+        }
+
+        public string md5(string input)
+        {
+                using var provider = System.Security.Cryptography.MD5.Create();
+                StringBuilder builder = new StringBuilder();
+
+                foreach (byte b in provider.ComputeHash(Encoding.UTF8.GetBytes(input)))
+                        builder.Append(b.ToString("x2").ToLower());
+
+                return builder.ToString();
+
+        }
+
+        public string gethash()
+        {
+                string data1 = this.sha256(this.csprng(4));
+                string data2 = this.sha256(this.csprng(8));
+                string data3 = this.sha256(this.unix());
+                string data4 = this.sha256(this.rand_int());
+                string data5 = this.sha256(this.rand_int_range(100));
+                string data6 = this.sha256(this.rand_int_range(300));
+
+                return this.md5(data1 + data2 + data3 + data4 + data5 + data6);
+        }
+
+
+}
+
+class main
+{
+        public static void Main()
+        {
+                generate_rand_str rs = new generate_rand_str();
+                //while (true) { Console.WriteLine(rs.unix()); }
+                while(true)
+                {
+                        Console.WriteLine(rs.gethash());
+                }
+        }
+}
