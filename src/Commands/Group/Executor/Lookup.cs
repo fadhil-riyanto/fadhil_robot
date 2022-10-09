789138f467bad44bc22a28ab58a02bafa7fb818b
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-10-09 14:11:27 +0700

    add catch while get something errors

--- src/Commands/Group/Executor/Lookup.cs
+++ src/Commands/Group/Executor/Lookup.cs
@@ -13,13 +13,14 @@ using Telegram.Bot;
 using fadhil_robot.Utils;
 using TL;
 
-namespace fadhil_robot.Commands.Group.Executor {
+namespace fadhil_robot.Commands.Group.Executor
+{
         class Lookup : Utils.IExecutor
         {
                 private InputTelegram _inputTelegram;
                 private ITelegramBotClient _botClient;
                 private Telegram.Bot.Types.Message _message;
-                public Lookup(InputTelegram inputTelegram, 
+                public Lookup(InputTelegram inputTelegram,
                         ITelegramBotClient botClient, Telegram.Bot.Types.Message message)
                 {
                         this._inputTelegram = inputTelegram;
@@ -28,17 +29,36 @@ namespace fadhil_robot.Commands.Group.Executor {
                 }
                 public async Task Execute()
                 {
-                        Contacts_ResolvedPeer peerdata = await this._inputTelegram.main_thread_ctx.ClientMT.Contacts_ResolveUsername(this._inputTelegram.value);
-                        string text = TranslateLocale.exec(
-                                this._message, "command.Group.Lookup", peerdata.User.first_name + " " + peerdata.User.last_name,
-                                peerdata.User.id.ToString(), peerdata.User.lang_code, peerdata.User.username, UtilsFN.is64(peerdata.User.id)
-                        );
-                        await this._botClient.SendTextMessageAsync(
-                                chatId: this._message.Chat.Id, 
-                                text: text, 
-                                replyToMessageId: this._message.MessageId, 
-                                parseMode: ParseMode.Html
-                        );
+                        try
+                        {
+                                Contacts_ResolvedPeer peerdata = await this._inputTelegram.main_thread_ctx.ClientMT.Contacts_ResolveUsername(this._inputTelegram.value);
+                                string text = TranslateLocale.exec(
+                                        this._message, "command.Group.Lookup", peerdata.User.first_name + " " + peerdata.User.last_name,
+                                        peerdata.User.id.ToString(), peerdata.User.lang_code, peerdata.User.username, UtilsFN.is64(peerdata.User.id)
+                                );
+                                await this._botClient.SendTextMessageAsync(
+                                        chatId: this._message.Chat.Id,
+                                        text: text,
+                                        replyToMessageId: this._message.MessageId,
+                                        parseMode: ParseMode.Html
+                                );
+                        }
+                        catch (RpcException e)
+                        {
+                                string error_result = e.Message switch
+                                {
+                                        "USERNAME_INVALID" => TranslateLocale.exec(this._message, "command.Group.Lookup.UsernameInvalid"),
+                                        _ => TranslateLocale.exec(this._message, "Unknown", e.Message.ToString())
+                                };
+                                await this._botClient.SendTextMessageAsync(
+                                        chatId: this._message.Chat.Id,
+                                        text: error_result,
+                                        replyToMessageId: this._message.MessageId,
+                                        parseMode: ParseMode.Html
+                                );
+                        }
+
+
                 }
         }
 }
