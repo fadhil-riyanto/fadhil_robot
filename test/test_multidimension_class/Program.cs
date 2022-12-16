ceed5993e283773edd29eacdd20ce24ab3f5c284
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-12-16 22:27:19 +0700

    just a test

---
+++ test/test_multidimension_class/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+
+class something_class
+{
+    public void print_hey(string data)
+    {
+        return $"hey {data}";
+    }
+}
+class main
+{
+    public static void Main()
+    {
+        something_class tsc = new(
+            new[] {
+                something_class
+            }
+        );
+    }
+}
+
+// 
