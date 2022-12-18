f3167bb03ceba50188f52825278ecf4eaf00923c
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-12-18 14:32:47 +0700

    fox codestyle

--- src/Utils/HelpKeyboard.cs
+++ src/Utils/HelpKeyboard.cs
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0
 
 /*
- *  Copyright (C) 2022 Fadhil Riyanto
- *
- *  https://github.com/fadhil-riyanto/fadhil_robot.git
- */
+*  Copyright (C) 2022 Fadhil Riyanto
+*
+*  https://github.com/fadhil-riyanto/fadhil_robot.git
+*/
 
 using Telegram.Bot.Types.ReplyMarkups;
 using Telegram.Bot.Types;
@@ -27,20 +27,20 @@ namespace fadhil_robot.Utils
         private Dictionary<string, string> _translations(string languange)
         {
             Dictionary<string, string> id_ID = new Dictionary<string, string> {
-                                {"admin" , "Admin"},
-                                {"utils" , "Alat-alat"},
+                {"admin" , "Admin"},
+                {"utils" , "Alat-alat"},
 
-                                // for back
-                                {"back" , "Kembali ◀️"}
-                        };
+                // for back
+                {"back" , "Kembali ◀️"}
+                };
 
             Dictionary<string, string> en_US = new Dictionary<string, string> {
-                                {"admin" , "Admin"},
-                                {"utils" , "Utils Tool"},
+                {"admin" , "Admin"},
+                {"utils" , "Utils Tool"},
 
-                                // for back
-                                {"back" , "Back ◀️"}
-                        };
+                // for back
+                {"back" , "Back ◀️"}
+                };
 
             if (languange == "id")
             {
@@ -61,24 +61,24 @@ namespace fadhil_robot.Utils
             Dictionary<string, string> data = this._translations(this._language);
 
             this._keyboard = new InlineKeyboardMarkup(
-                new InlineKeyboardButton[][] {
+            new InlineKeyboardButton[][] {
                     new InlineKeyboardButton[] {
                         InlineKeyboardButton.WithCallbackData(
-                            text: data["admin"], callbackData: CallbackHelper.pack(
-                                            this._inputTelegram, "help", new Dictionary<string, string> {
-                                            { "clicked_button", "admin"}
-                                    }
+                                text: data["admin"], callbackData: CallbackHelper.pack(
+                                    this._inputTelegram, "help", new Dictionary<string, string> {
+                                    { "clicked_button", "admin"}
+                                }
                             )
                         ),
                         InlineKeyboardButton.WithCallbackData(
                             text: data["utils"], callbackData: CallbackHelper.pack(
-                                            this._inputTelegram, "help", new Dictionary<string, string> {
-                                            { "clicked_button", "utils"}
-                                    }
+                                    this._inputTelegram, "help", new Dictionary<string, string> {
+                                    { "clicked_button", "utils"}
+                                }
                             )
                         ),
                     }
-            }
+                }
             );
             return this;
         }
@@ -88,19 +88,18 @@ namespace fadhil_robot.Utils
             Dictionary<string, string> data = this._translations(this._language);
 
             this._keyboard = new(new[]
-                    {
-                                        new []
-                                        {
-                                                InlineKeyboardButton.WithCallbackData(
-
-                                                        text: data["back"], callbackData: CallbackHelper.pack(
-                                                                        this._inputTelegram, "help_back", new Dictionary<string, string> {
-                                                                        { "clicked_button", "back"}
-                                                                }
-                                                        )
-                                                ),
-                                        }
-                                }
+            {
+                new []
+                {
+                    InlineKeyboardButton.WithCallbackData(
+                        text: data["back"], callbackData: CallbackHelper.pack(
+                                    this._inputTelegram, "help_back", new Dictionary<string, string> {
+                                    { "clicked_button", "back"}
+                            }
+                        )
+                    ),
+                },
+            }
             );
             return this;
         }
@@ -113,34 +112,34 @@ namespace fadhil_robot.Utils
         public string getContent(string keys)
         {
             Dictionary<string, string> id_ID = new Dictionary<string, string> {
-                                {"admin" ,
-                                        "*Admin*:" + ITgKeyboard.DOUBLE_NL + ITgKeyboard.DOUBLE_NL +
-                                        "/pin: sematkan pesan kamu" + ITgKeyboard.NL +
-                                        "/unpin: melepas sematan grup" + ITgKeyboard.NL +
-                                        "/adminlist: melihat daftar admin di grup saat ini" + ITgKeyboard.NL
-                                },
-                                {"utils" ,
-                                        "Alat Alat\n\n" +
-                                        "Dibawah ini adalah command yang tersedia untuk membantumu dalam mencari suatu info" + ITgKeyboard.DOUBLE_NL +
-                                        "/whoami: digunakan untuk melihat info tentang dirimu sendiri" + ITgKeyboard.NL +
-                                        "/lookup: digunakan untuk mencari pengguna telegram berdasarkan username / id" + ITgKeyboard.NL
-                                }
-                        };
+                {"admin" ,
+                "*Admin*:" + ITgKeyboard.DOUBLE_NL + ITgKeyboard.DOUBLE_NL +
+                "/pin: sematkan pesan kamu" + ITgKeyboard.NL +
+                "/unpin: melepas sematan grup" + ITgKeyboard.NL +
+                "/adminlist: melihat daftar admin di grup saat ini" + ITgKeyboard.NL
+                },
+                {"utils" ,
+                "Alat Alat\n\n" +
+                "Dibawah ini adalah command yang tersedia untuk membantumu dalam mencari suatu info" + ITgKeyboard.DOUBLE_NL +
+                "/whoami: digunakan untuk melihat info tentang dirimu sendiri" + ITgKeyboard.NL +
+                "/lookup: digunakan untuk mencari pengguna telegram berdasarkan username / id" + ITgKeyboard.NL
+                }
+            };
 
             Dictionary<string, string> en_US = new Dictionary<string, string> {
-                                {"admin" ,
-                                        "*Admins*:" + ITgKeyboard.DOUBLE_NL + ITgKeyboard.DOUBLE_NL +
-                                        "/pin: pins your messange" + ITgKeyboard.NL +
-                                        "/unpin: unpins the currently pinned messange" + ITgKeyboard.NL +
-                                        "/adminlist: get admin list in current group" + ITgKeyboard.NL
-                                },
-                                {"utils" ,
-                                        "Utils tools" +
-                                        "Below are the commands available to help you search something information" + ITgKeyboard.DOUBLE_NL +
-                                        "/whoami: can be used for see your telegram account info" + ITgKeyboard.NL +
-                                        "/lookup: used to search for telegram users based on username / id" + ITgKeyboard.NL
-                                }
-                        };
+                {"admin" ,
+                "*Admins*:" + ITgKeyboard.DOUBLE_NL + ITgKeyboard.DOUBLE_NL +
+                "/pin: pins your messange" + ITgKeyboard.NL +
+                "/unpin: unpins the currently pinned messange" + ITgKeyboard.NL +
+                "/adminlist: get admin list in current group" + ITgKeyboard.NL
+                },
+                {"utils" ,
+                "Utils tools" +
+                "Below are the commands available to help you search something information" + ITgKeyboard.DOUBLE_NL +
+                "/whoami: can be used for see your telegram account info" + ITgKeyboard.NL +
+                "/lookup: used to search for telegram users based on username / id" + ITgKeyboard.NL
+                }
+            };
 
             if (this._language == "id")
             {
