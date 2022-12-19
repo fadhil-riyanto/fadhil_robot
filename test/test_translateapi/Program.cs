482915d539ec13d2fdd1020a884eac294cb04baa
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-12-19 19:33:11 +0700

    test libretranslate api

---
+++ test/test_translateapi/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+class mainku
+{
+    private static readonly HttpClient client = new HttpClient();
+
+    public async static Task Main()
+    {
+        string text = "komputer adalah mesin yang dilengkapi CPU berkecapaan tinggi";
+        string source = "id";
+        string target = "en";
+        HttpClient client = new HttpClient();
+        var values = new Dictionary<string, string>
+            {
+                { "q", text },
+                { "source",  source },
+                { "target", target }
+            };
+
+        var content = new FormUrlEncodedContent(values);
+        var response = await client.PostAsync("https://libretranslate.de/translate", content);
+
+        JObject responseString = JObject.Parse(await response.Content.ReadAsStringAsync());
+        System.Console.WriteLine(responseString["translatedText"]);
+        //return responseString["translatedText"];
+
+    }
+
+}
+
+
