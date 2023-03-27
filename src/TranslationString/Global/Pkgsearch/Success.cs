934cc903d97ae4eec799994063f37083536a48dd
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2023-03-27 22:36:53 +0700

    fix format error

--- src/TranslationString/Global/Pkgsearch/Success.cs
+++ src/TranslationString/Global/Pkgsearch/Success.cs
@@ -5,13 +5,13 @@ namespace fadhil_robot.TranslationString.Global.Pkgsearch
     class Success : Utils.translate_string_parent{
         public override string translate_en_US {
             get {
-                return "{0} ({1})\n\nDesc: {2}\n ";
+                return "{0} ({1})\n\nDesc: {2}\nLast update: {3})";
             }
         }
 
         public override string translate_id_ID {
             get {
-                return "{0} ({1})\n\nDeskripsi: {2}\n ";
+                return "{0} ({1})\n\nDeskripsi: {2}\nTerakhir update: {3}) ";
             }
         }
     }
